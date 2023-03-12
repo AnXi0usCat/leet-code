d8d6610a3b61d5c6ad0d164249965c7fa5ce1062
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-03-12 18:19:35 +0000

    reformat and unit tests

--- add_two_numbers/src/main.rs
+++ add_two_numbers/src/main.rs
@@ -1,18 +1,141 @@
-
 fn main() {
     println!("Add two numbers");
 }
 
 // Definition for singly-linked list.
- #[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct ListNode {
     pub val: i32,
-    pub next: Option<Box<ListNode>>
+    pub next: Option<Box<ListNode>>,
 }
- 
+
 impl ListNode {
     #[inline]
     fn new(val: i32) -> Self {
-        ListNode { next: None, val }
+        ListNode { val, next: None }
+    }
+}
+
+trait AddTwoNumbers {
+    fn solution(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>>;
+}
+
+struct Solution;
+
+impl AddTwoNumbers for Solution {
+    fn solution(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let carry = 0;
+        let result = ListNode::new(0);
+
+        let p1 = &l1;
+        let p2 = &l2;
+
+        while *p1 != None && *p2 != None {
+            let sum = carry;
+
+            if let Some(val1) = *p1 {}
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution_1() {
+        let l1 = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: Some(Box::new(ListNode { val: 3, next: None })),
+            })),
+        }));
+        let l2 = Some(Box::new(ListNode {
+            val: 5,
+            next: Some(Box::new(ListNode {
+                val: 6,
+                next: Some(Box::new(ListNode { val: 4, next: None })),
+            })),
+        }));
+        assert_eq!(
+            Solution::solution(l1, l2),
+            Some(Box::new(ListNode {
+                val: 7,
+                next: Some(Box::new(ListNode {
+                    val: 0,
+                    next: Some(Box::new(ListNode { val: 8, next: None }))
+                }))
+            }))
+        );
+    }
+
+    #[test]
+    fn solution_2() {
+        let l1 = Some(Box::new(ListNode { val: 0, next: None }));
+        let l2 = Some(Box::new(ListNode { val: 0, next: None }));
+        assert_eq!(
+            Solution::solution(l1, l2),
+            Some(Box::new(ListNode { val: 0, next: None }))
+        );
+    }
+
+    #[test]
+    fn solution_3() {
+        let l1 = Some(Box::new(ListNode {
+            val: 9,
+            next: Some(Box::new(ListNode {
+                val: 9,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: Some(Box::new(ListNode {
+                        val: 9,
+                        next: Some(Box::new(ListNode {
+                            val: 9,
+                            next: Some(Box::new(ListNode {
+                                val: 9,
+                                next: Some(Box::new(ListNode { val: 9, next: None })),
+                            })),
+                        })),
+                    })),
+                })),
+            })),
+        }));
+        let l2 = Some(Box::new(ListNode {
+            val: 9,
+            next: Some(Box::new(ListNode {
+                val: 9,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: Some(Box::new(ListNode { val: 9, next: None })),
+                })),
+            })),
+        }));
+        assert_eq!(
+            Solution::solution(l1, l2),
+            Some(Box::new(ListNode {
+                val: 8,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: Some(Box::new(ListNode {
+                        val: 9,
+                        next: Some(Box::new(ListNode {
+                            val: 9,
+                            next: Some(Box::new(ListNode {
+                                val: 0,
+                                next: Some(Box::new(ListNode {
+                                    val: 0,
+                                    next: Some(Box::new(ListNode {
+                                        val: 0,
+                                        next: Some(Box::new(ListNode { val: 0, next: None }))
+                                    }))
+                                }))
+                            }))
+                        }))
+                    }))
+                }))
+            }))
+        );
     }
 }

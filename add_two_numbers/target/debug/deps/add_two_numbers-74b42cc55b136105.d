5493c7dff7513f6e177d848d45b028d4204d2d75
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-03-12 12:59:42 +0000

    remove target directory

--- add_two_numbers/target/debug/deps/add_two_numbers-74b42cc55b136105.d
+++
@@ -1,5 +0,0 @@
-/Users/mihhailf/rust/leet-code/add_two_numbers/target/debug/deps/add_two_numbers-74b42cc55b136105.rmeta: src/main.rs
-
-/Users/mihhailf/rust/leet-code/add_two_numbers/target/debug/deps/add_two_numbers-74b42cc55b136105.d: src/main.rs
-
-src/main.rs:

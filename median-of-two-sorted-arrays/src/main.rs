7a706f76616be254da3861c52815b8a1032ec4ea
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-03-15 19:54:50 +0000

    reformat code

--- median-of-two-sorted-arrays/src/main.rs
+++ median-of-two-sorted-arrays/src/main.rs
@@ -2,21 +2,18 @@ fn main() {
     println!("median-of-two-sorted-arrays");
 }
 
-
 trait Solution {
     fn solution(nums1: Vec<i32>, nums2: Vec<i32>) -> f64;
 }
 
 struct CheatingSolution;
 
-
 impl Solution for CheatingSolution {
     fn solution(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        
         // make vectors mutable so we could merge and sort them
         let mut nums1_mut = nums1;
         let mut nums2_mut = nums2;
-        
+
         nums1_mut.append(&mut nums2_mut);
         nums1_mut.sort();
 
@@ -25,7 +22,7 @@ impl Solution for CheatingSolution {
         let quotient = num_len / 2;
 
         if num_len % 2 == 0 {
-            median = (nums1_mut[quotient - 1] as f64 + nums1_mut[quotient] as f64) / 2.0; 
+            median = (nums1_mut[quotient - 1] as f64 + nums1_mut[quotient] as f64) / 2.0;
         } else {
             median = nums1_mut[num_len / 2] as f64;
         }
@@ -34,18 +31,16 @@ impl Solution for CheatingSolution {
     }
 }
 
-
 #[cfg(test)]
 mod test {
     use super::*;
-   
+
     #[test]
     fn solution_1() {
         let in1 = vec![1, 3];
         let in2 = vec![2];
         assert_eq!(CheatingSolution::solution(in1, in2), 2 as f64);
     }
-
 
     #[test]
     fn solution_2() {

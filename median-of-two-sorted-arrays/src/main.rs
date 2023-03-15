0a88e86ce72a7d8692ccc370758a348619d3d1ba
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-03-15 09:43:38 +0000

    scafolding for the median-of-two-sorted-arrays

---
+++ median-of-two-sorted-arrays/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("median-of-two-sorted-arrays");
+}
+
+
+trait Solution {
+    fn solution(nums1: Vec<i32>, nums2: Vec<i32>) -> f64;
+}
+
+struct BinarySearch;
+
+
+impl Solution for BinarySearch {
+    fn solution(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+
+        0 as f64
+    }
+}
